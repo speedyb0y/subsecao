667bb1feaa668cb5854520bdf0681b6ea48b1737
Willian B <speedyb0y@users.noreply.github.com>
2021-02-23 07:56:31 -0300

    Update remote.py

--- remote.py
+++ remote.py
@@ -3,12 +3,26 @@
 import sys
 import os
 import time
+import fcntl
 
-def pipe (cmd):
+# echo $[2*1024*1024*1024] > /proc/sys/fs/pipe-max-size || exit 1
+# echo $[2*1024*1024*1024] > /proc/sys/fs/pipe-user-pages-hard || exit 1
+# echo $[2*1024*1024*1024] > /proc/sys/fs/pipe-user-pages-soft || exit 1
+
+F_SETPIPE_SZ = 1031
+F_GETPIPE_SZ = 1032
+
+def pipe (buffSize, cmd):
 
     r, w = os.pipe()
 
-    if not os.fork():
+    fcntl.fcntl(r,  F_SETPIPE_SZ, buffSize)
+
+    assert int(fcntl.fcntl(r,  F_GETPIPE_SZ)) == buffSize
+
+    pid = os.fork()
+
+    if not pid:
         os.close(1)
         os.close(r)
         os.dup2(w, 1)
@@ -17,30 +31,34 @@ def pipe (cmd):
 
     os.close(w)
 
-    # TODO: FIXME: KEEP READING
-    x = os.read(r, 128*1024*1024)[:-1]
+    _, status = os.waitpid(pid, 0)
+
+    assert _ == pid
+    assert status == 0
+
+    data = os.read(r, buffSize)
 
     os.close(r)
 
-    return x
+    return data
 
 # TSserver.corenrj.local
-remoteIP='172.16.0.26'
+remoteHost ='172.16.0.26'
 remotePort = 3389
 remoteWidth = 880
 remoteHeight = 730
 remoteDomain = 'CORENRJ'
 remoteUser = 'willianbrito'
-remotePwd = pipe('zenity --password').decode()
+remotePwd = pipe(4096, 'zenity --password')[:-1].decode()
 
 printDir = './IMPRIMIR'
 
 assert isinstance(printDir, str) and printDir
-assert isinstance(remoteIP, str) and remoteIP
+assert isinstance(remoteHost, str) and remoteHost
+assert isinstance(remotePort, int) and 0 <= remotePort <= 0xFFFF
 assert isinstance(remoteDomain, str)
 assert isinstance(remoteUser, str) and remoteUser
 assert isinstance(remotePwd, str) and remotePwd
-assert isinstance(remotePort, int) and 0 <= remotePort <= 0xFFFF
 assert 100 <= remoteWidth <= 20000
 assert 100 <= remoteHeight <= 20000
 
@@ -57,7 +75,7 @@ while True:
     # TODO: FIXME: IR ALTERNANDO
     if True:
 
-        os.system('xfreerdp'
+        os.system('xfreerdp '
             '/t:INCORP '
             '+clipboard '
             '-themes '
@@ -69,7 +87,7 @@ while True:
             f'/d:{remoteDomain} '
             f'/u:{remoteUser} '
             f'/p:{remotePwd} '
-            f'/v:{remoteIP}:{remotePort} '
+            f'/v:{remoteHost}:{remotePort} '
             f'/w:{remoteWidth} '
             f'/h:{remoteHeight} '
             f'/window-position:{remoteX}x0 '
@@ -77,6 +95,6 @@ while True:
             )
     else:
 
-        os.system(f'rdesktop -D -T INCORP {remoteIP}:{remotePort} -g {remoteWidth}x{remoteHeight}+{remoteX}+0 -u {remoteDomain}\\{remoteUser} -p {remotePwd} -r disk:IMPRIMIR={printDir} -M -a 8 -z -P -x m')
+        os.system(f'rdesktop -D -T INCORP {remoteHost}:{remotePort} -g {remoteWidth}x{remoteHeight}+{remoteX}+0 -u {remoteDomain}\\{remoteUser} -p {remotePwd} -r disk:IMPRIMIR={printDir} -M -a 8 -z -P -x m')
 
     time.sleep(2)
